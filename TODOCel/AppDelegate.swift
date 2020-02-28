4cc7f3ddc8437326f6be9fcee5c4fb49afad5f69
celso lima <celso.lima@evtit.com>
2020-02-28 12:00:24 -0300

    studying application life cycle methods

--- TODOCel/AppDelegate.swift
+++ TODOCel/AppDelegate.swift
@@ -15,7 +15,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("application - didFinishLaunchingWithOptions")
+        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        
         return true
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("applicationDidBecomeActive")
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("applicationDidBecomeActive")
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("applicationDidBecomeActive")
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("applicationDidBecomeActive")
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("applicationDidBecomeActive")
     }
 
     // MARK: UISceneSession Lifecycle
